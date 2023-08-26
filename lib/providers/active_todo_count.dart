a82f83d3bc3111c847c918d412a510a6eee72e89
Sam Victor <iamvictorsam@gmail.com>
2023-08-26 15:41:08 +0100

    added todo count

---
+++ lib/providers/active_todo_count.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/widgets.dart';
+import 'package:todo_provider/models/todo_model.dart';
+import 'package:todo_provider/providers/todo_list.dart';
+
+class ActiveTodoCountState {
+  final int activeTodoCount;
+
+  ActiveTodoCountState({required this.activeTodoCount});
+
+  factory ActiveTodoCountState.initial() {
+    return ActiveTodoCountState(activeTodoCount: 0);
+  }
+
+  @override
+  String toString() =>
+      'ActiveTodoCountState(activeTodoCount: $activeTodoCount)';
+
+  @override
+  bool operator ==(covariant ActiveTodoCountState other) {
+    if (identical(this, other)) return true;
+
+    return other.activeTodoCount == activeTodoCount;
+  }
+
+  @override
+  int get hashCode => activeTodoCount.hashCode;
+
+  ActiveTodoCountState copyWith({
+    int? activeTodoCount,
+  }) {
+    return ActiveTodoCountState(
+      activeTodoCount: activeTodoCount ?? this.activeTodoCount,
+    );
+  }
+}
+
+class ActiveTodoCount with ChangeNotifier {
+  ActiveTodoCountState _state = ActiveTodoCountState.initial();
+  ActiveTodoCountState get state => _state;
+
+  void update(TodoList todoList) {
+    final int newActiveTodoCount = todoList.state.todos
+        .where((Todo todo) => !todo.completed)
+        .toList()
+        .length;
+
+    _state = _state.copyWith(activeTodoCount: newActiveTodoCount);
+    notifyListeners();
+  }
+}
